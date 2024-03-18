20f195c48653cfc6f71fd19fea722b3db9221680
Heder David <hedermd6@gmail.com>
2024-03-18 12:25:46 -0300

    ex05 finalizado

--- src/exercicio05/Cronometro.java
+++ src/exercicio05/Cronometro.java
@@ -1,2 +1,35 @@
-package exercicio05;public class Cronometro {
+package exercicio05;
+
+import static java.lang.Thread.sleep;
+
+public class Cronometro implements Runnable {
+    private long inicioContagem;
+    private final long MILISEGUNDOS;
+    private long contador;
+
+    public Cronometro(long inicioContagem, long milisegundos) {
+        this.inicioContagem = inicioContagem;
+        MILISEGUNDOS = milisegundos;
+        contador = inicioContagem;
+
+    }
+    @Override
+    public void run() {
+        for (long i = inicioContagem; i > 0; i--) {
+            try {
+                sleep(MILISEGUNDOS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            contador--;
+        }
+    }
+
+    public long getContador() {
+        return contador;
+    }
+
+    public String getSituacao() {
+        return contador == 0 ? "<encerrado>" : "<em atividade>";
+    }
 }
--- src/exercicio05/Main.java
+++ src/exercicio05/Main.java
@@ -1,2 +1,54 @@
-package exercicio05;public class Main {
+package exercicio05;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Cronometro[] cronometros = new Cronometro[3];
+        long tempo;
+        String escolha = null;
+        char selecionarCronometro;
+        boolean isF = false;
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < 3; i++) {
+            System.out.print( (i + 1) + "ª Contagem Regressiva..............: ");
+            tempo = scanner.nextLong();
+            scanner.nextLine();
+            System.out.print("Pausa P/" + (i + 1) + "ª Contagem Regressiva (S/N)? ");
+            escolha = scanner.nextLine();
+            cronometros[i] = new Cronometro(tempo, escolha.equalsIgnoreCase("s") ? 1000 : 0 );
+            Thread t  = new Thread(cronometros[i]);
+            t.start();
+        }
+
+        do {
+            System.out.print("Consultar Contador Regressivo (1/2/3) ou Encerrar (F)? ");
+            selecionarCronometro = scanner.next().charAt(0);
+
+            int opcao = Character.getNumericValue(selecionarCronometro);
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Contagem Atual de Contador 1 -> " + cronometros[0].getContador() + " " + cronometros[0].getSituacao());
+                    break;
+                case 2:
+                    System.out.println("Contagem Atual de Contador 2 -> " + cronometros[1].getContador() + " " + cronometros[1].getSituacao());
+                    break;
+                case 3:
+                    System.out.println("Contagem Atual de Contador 3 -> " + cronometros[2].getContador() + " " + cronometros[2].getSituacao());
+                    break;
+                default:
+                    isF = true;
+                    if (cronometros[0].getContador() != 0 || cronometros[1].getContador() != 0 || cronometros[3].getContador() != 0) {
+                        System.out.println("O programa se encerrará assim que todos os cronometros chegarem a 0");
+                    } else {
+                        System.out.println("Programa encerrado.");
+                    }
+                    break;
+            }
+            scanner.nextLine();
+        } while (!isF);
+
+        scanner.close();
+    }
 }
